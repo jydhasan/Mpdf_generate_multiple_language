c2d8d58b7e3507f6d4a35ba55e37a254402bb6f8
jydhasan <s1910933181@ru.ac.bd>
2024-02-11 10:37:32 +0600

    first commit

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PDF</h1>
+    <p>PDF content goes here</p>
+    <p style="font-family:freeserif"> আমার সোনার বাংলা আমি তোমায় ভালোবাসি </p>
+</body>
+</html>
